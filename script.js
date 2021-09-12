a75e785c19f06fe3c69c92ca85a9565b84a72cdc
dream-girl888 <87891649+dream-girl888@users.noreply.github.com>
2021-09-12 15:29:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hallo Winc Academy studenten")
+
+//Dit is een grote som
+//de bovenstaande comment zie je niet als je runt met node want het is een commentaar
+let vermenigvuldigen = 1230941 * 1823794;
+console.log("Vermenigvuldigen", vermenigvuldigen)
+
+let delen = 637263 / 54;
+console.log("Delen", delen)
+
+//je ziet de onderstaande code niet als je blok comment doet.
+/*let vermenigvuldigen = 1230941 * 1823794;
+console.log("Vermenigvuldigen", vermenigvuldigen)
+
+let delen = 637263 / 54;
+console.log("Delen", delen)*/
